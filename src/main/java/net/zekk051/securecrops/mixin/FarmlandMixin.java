3e2871b374f64cd77f6c666bf24f102895d4e1ea
zekk051 <97740808+zekk051@users.noreply.github.com>
2023-05-17 16:05:08 -0300

    1.16.5 release
    Java compatibility level set to 8

--- src/main/java/net/zekk051/securecrops/mixin/FarmlandMixin.java
+++ src/main/java/net/zekk051/securecrops/mixin/FarmlandMixin.java
@@ -2,11 +2,10 @@ package net.zekk051.securecrops.mixin;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.FarmlandBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.zekk051.securecrops.SecureCrops;
@@ -20,17 +19,17 @@ public abstract class FarmlandMixin extends Block {
     // Modified code from https://github.com/GitWither/feather-trampling
     public FarmlandMixin(Settings settings) { super(settings); }
 
-    @Inject(method="onLandedUpon", cancellable = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/entity/Entity;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"))
-    public void cancelTrample(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
+    @Inject(method="onLandedUpon", cancellable = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"))
+    public void cancelTrample(World world, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
 
         if(world.getGameRules().getBoolean(SecureCrops.SECURE_CROPS) && !world.getBlockState(pos.up()).isAir()) {
-            super.onLandedUpon(world, state, pos, entity, fallDistance);
+            super.onLandedUpon(world, pos, entity, fallDistance);
             ci.cancel();
             printDebugMsg("[Debug] Prevented crop trampling", entity);
             return;
         }
         if(world.getGameRules().getBoolean(SecureCrops.SECURE_FARMLAND) && world.getBlockState(pos.up()).isAir()) {
-            super.onLandedUpon(world, state, pos, entity, fallDistance);
+            super.onLandedUpon(world, pos, entity, fallDistance);
             ci.cancel();
             printDebugMsg("[Debug] Prevented farmland trampling", entity);
             return;
@@ -39,7 +38,7 @@ public abstract class FarmlandMixin extends Block {
 
     public void printDebugMsg(String message, Entity entity) {
         if (FabricLoader.getInstance().isDevelopmentEnvironment() && entity instanceof PlayerEntity) {
-            ((PlayerEntity) entity).sendMessage(Text.literal(message), false);
+            ((PlayerEntity) entity).sendMessage(new LiteralText(message), false);
         }
     }
 }
