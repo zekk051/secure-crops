30c1f3617e211de84e757e7b823f7f10d8e2f145
zekk051 <97740808+zekk051@users.noreply.github.com>
2023-08-08 03:22:56 -0300

    v1.0.4
    Added secureFarmlandWithCrops gamerule

--- src/main/java/net/zekk051/securecrops/mixin/FarmlandMixin.java
+++ src/main/java/net/zekk051/securecrops/mixin/FarmlandMixin.java
@@ -7,9 +7,11 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.zekk051.securecrops.SecureCrops;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,21 +21,42 @@ public abstract class FarmlandMixin extends Block {
     // Modified code from https://github.com/GitWither/feather-trampling
     public FarmlandMixin(Settings settings) { super(settings); }
 
+    @Shadow
+    private static boolean hasCrop(BlockView world, BlockPos pos) {
+        return false;
+    }
+
     @Inject(method="onLandedUpon", cancellable = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"))
     public void cancelTrample(World world, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
+        boolean GAMERULE_SECURE_CROPS = world.getGameRules().getBoolean(SecureCrops.SECURE_CROPS);
+        boolean GAMERULE_SECURE_FARMLAND_WITH_CROPS = world.getGameRules().getBoolean(SecureCrops.SECURE_FARMLAND_WITH_CROPS);
+        boolean GAMERULE_SECURE_FARMLAND = world.getGameRules().getBoolean(SecureCrops.SECURE_FARMLAND);
+        boolean hasCrop = hasCrop(world, pos);
 
-        if(world.getGameRules().getBoolean(SecureCrops.SECURE_CROPS) && !world.getBlockState(pos.up()).isAir()) {
+        if(hasCrop && GAMERULE_SECURE_CROPS) {
             super.onLandedUpon(world, pos, entity, fallDistance);
             ci.cancel();
             printDebugMsg("[Debug] Prevented crop trampling", entity);
             return;
         }
-        if(world.getGameRules().getBoolean(SecureCrops.SECURE_FARMLAND) && world.getBlockState(pos.up()).isAir()) {
+
+        if(hasCrop && GAMERULE_SECURE_FARMLAND_WITH_CROPS) {
+            ci.cancel();
+            breakCrop(world, pos, entity);
+            printDebugMsg("[Debug] Trampled crop, secure farmland", entity);
+            return;
+        }
+
+        if(!hasCrop && GAMERULE_SECURE_FARMLAND) {
             super.onLandedUpon(world, pos, entity, fallDistance);
             ci.cancel();
             printDebugMsg("[Debug] Prevented farmland trampling", entity);
             return;
         }
+    }
+
+    public void breakCrop(World world, BlockPos pos, Entity entity) {
+        world.breakBlock(pos.up(), true, entity);
     }
 
     public void printDebugMsg(String message, Entity entity) {
