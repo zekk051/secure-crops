7fdc8b7fac6ae7245337d3f615a913d698d7aec5
Silencio <dev.silencio@gmail.com>
2024-08-12 14:26:24 +0200

    Update to 1.21, Code cleanup

--- src/main/java/net/zekk051/securecrops/SecureCrops.java
+++ src/main/java/net/zekk051/securecrops/SecureCrops.java
@@ -7,21 +7,20 @@ import net.minecraft.world.GameRules;
 import net.minecraft.world.GameRules.BooleanRule;
 
 public class SecureCrops implements ModInitializer {
+    // Modified code from https://github.com/A5b84/convenient-mobgriefing
+    public static final GameRules.Key<BooleanRule>
+            SECURE_CROPS = register("secureCrops", false), // Do not break crop or farmland when farmland has crops
+            SECURE_FARMLAND_WITH_CROPS = register("secureFarmlandWithCrops", false),	// Break crop but leave farmland when farmland has crops
+            SECURE_FARMLAND = register("secureFarmland", false); // Do not break farmland without crops
 
-	// Modified code from https://github.com/A5b84/convenient-mobgriefing
-	public static final GameRules.Key<BooleanRule>
-			SECURE_CROPS = register("secureCrops", true),
-			SECURE_FARMLAND_WITH_CROPS = register("secureFarmlandWithCrops", true),	// Break crop and leave farmland the same
-			SECURE_FARMLAND = register("secureFarmland", false);
+    private static GameRules.Key<BooleanRule> register(String name, boolean boolValue) {
+        return GameRuleRegistry.register(
+                name, GameRules.Category.MISC,
+                GameRuleFactory.createBooleanRule(boolValue)
+        );
+    }
 
-	private static GameRules.Key<BooleanRule> register(String name, boolean boolValue) {
-		return GameRuleRegistry.register(
-				name, GameRules.Category.MISC,
-				GameRuleFactory.createBooleanRule(boolValue)
-		);
-	}
-
-	@Override
-	public void onInitialize() {
-	}
+    @Override
+    public void onInitialize() {
+    }
 }
