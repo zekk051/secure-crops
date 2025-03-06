357cbfd0f07c27812a07372f67917bae2ac085e6
zekk051 <leoezesosa@gmail.com>
2025-03-05 21:46:35 -0300

    first commit: stonecutter template

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.5"
+}
+
+stonecutter {
+    kotlinController = true
+    centralScript = "build.gradle.kts"
+
+    shared {
+        versions("1.20.1", "1.20.4", "1.20.6", "1.21.1")
+    }
+    create(rootProject)
+}
+
+rootProject.name = "Template"
