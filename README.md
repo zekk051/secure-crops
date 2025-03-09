374848f8964c0ef2977e9fa7832e01c230c94470
zekk051 <leoezesosa@gmail.com>
2025-03-09 12:15:15 -0300

    add gamerules category

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ### Todo:
-- Add gamerules group
 - Add gamerules description: in the 'Edit Game Rules' menu (world creation) there's a description for each gamerule, visible on hover
 - New gamerule only break mature crops
 - Add videos/gifs in modrinth and curseforge
--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -1,13 +1,22 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.CustomGameRuleCategory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.GameRules.BooleanRule;
 
 public class TemplateMod implements ModInitializer {
-    // Modified code from https://github.com/A5b84/convenient-mobgriefing
+    // Modified code from:
+    // https://github.com/A5b84/convenient-mobgriefing
+    // https://github.com/FabricMC/fabric/tree/1.21.4/fabric-game-rule-api-v1/src/testmod
+
+    public static final CustomGameRuleCategory SECURE_CROPS_CATEGORY = new CustomGameRuleCategory(Identifier.of("secure_crops", "gamerule_category"), Text.literal("Secure Crops").styled(style -> style.withBold(true).withColor(Formatting.YELLOW)));
+
     public static final GameRules.Key<BooleanRule>
             SECURE_FARMLAND_AND_CROPS = register("secureFarmlandAndCrops"), // If crop present, secure farmland and crop
             SECURE_FARMLAND_BREAK_CROPS = register("secureFarmlandBreakCrops"),	// If crop present, secure farmland but trample crop
@@ -15,7 +24,7 @@ public class TemplateMod implements ModInitializer {
 
     private static GameRules.Key<BooleanRule> register(String name) {
         return GameRuleRegistry.register(
-                name, GameRules.Category.MISC,
+                name, SECURE_CROPS_CATEGORY,
                 GameRuleFactory.createBooleanRule(false)
         );
     }
